afe0c1730898c366db0a961da44c067df98e6e6f
miguelsett <miguelsettimio@gmail.com>
2025-05-20 10:43:48 -0300

    criando o modelo pessoa

---
+++ src/models/pessoa.js
@@ -0,0 +1,30 @@
+// npx sequelize-cli model:generate --name Pessoa --attributes nome:string,email:string,cpf:string,ativo:boolean,role:string
+
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pessoa extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Pessoa.init({
+    nome: DataTypes.STRING,
+    email: DataTypes.STRING,
+    cpf: DataTypes.STRING,
+    ativo: DataTypes.BOOLEAN,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Pessoa',
+    tableName: 'pessoas'
+  });
+  return Pessoa;
+};
