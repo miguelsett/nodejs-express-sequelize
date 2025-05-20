00595fc6790115f997bbd6fc8cd83d7ae173295e
miguelsett <miguelsettimio@gmail.com>
2025-05-20 13:31:44 -0300

    criando rota e controller

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,11 @@
+class PessoaController {
+  static async pegaTodas (req, res) {
+    try {
+      // acessar a rota
+    } catch (error) {
+      // erro
+    }
+  }
+}
+
+module.exports = PessoaController;
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const pessoas = require('./pessoasRoute');
+
+module.exports = (app) => {
+  app.use(
+    express.json(),
+    pessoas
+  );
+};
