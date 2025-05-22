fee15b8f859df6f7e00fca6f694d2a65b3a65fab
st-mirighetto <141861843+st-mirighetto@users.noreply.github.com>
2025-05-22 11:14:14 -0300

    Finalizando CRUDs

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -11,6 +11,49 @@ class Controller {
       // erro
     }
   }
+
+  async pegaUmPorId(req, res) {
+    const { id } = req.params;
+    try {
+      const registro = await this.entidadeService.pegaUmRegistroPorId(id);
+      return res.status(200).json(umRegistro);
+    } catch (erro) {
+      // erro
+    }
+  }
+
+  async criaNovo(req, res) {
+    const novoRegistro = req.body;
+    try {
+      const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosNovos);
+      return res.status(200).json(novoRegistroCriado);
+    } catch (erro) {
+      // erro
+    }
+  }
+  async atualiza(req, res) {
+    const { id } = req.params;
+    const dadosAtualizados = req.body;
+    try {
+      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
+      if (!foiAtualizado) {
+        return res.status(400).json({ message: 'Registro não foi atualizado' });
+      }
+      return res.status(200).json({ message: 'Registro atualizado com sucesso' });
+    } catch (erro) {
+      // erro
+    }
+  }
+
+  async exclui(req, res) {
+    const { id } = req.params;
+    try {
+      await this.entidadeService.apagaRegistro(Number(id));
+      return res.status(200).json({ message: `id ${id} deletado` });
+    } catch (erro) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = Controller;
--- src/services/Services.js
+++ src/services/Services.js
@@ -8,6 +8,28 @@ class Services {
   async pegaTodosOsRegistros() {
     return dataSource[this.model].findAll();
   }
+
+  async pegaUmRegistroPorId(id) {
+    return dataSource[this.model].findByPk(id);
+  }
+
+  async criaRegistro(dados) {
+    return dataSource[this.model].create(dadosDoRegistro);
+  }
+
+  async atualizaRegistro(id, dados) {
+    const listaDeRegistrosAtualizados = dataSource[this.model].update(dadosAtulizados, {
+      where: { id: id },
+    });
+    if(listaDeRegistrosAtualizados[0] === 0) {
+      return false;
+    }
+    return true;
+  }
+
+  async apagaRegistro(id) {
+    return dataSource[this.model].destroy({ where: { id: id } });
+  }
 }
 
 module.exports = Services;
