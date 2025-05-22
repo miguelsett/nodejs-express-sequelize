5b156556855dbfd4d255fc8236120179d2093d9a
st-mirighetto <141861843+st-mirighetto@users.noreply.github.com>
2025-05-22 11:25:06 -0300

    adicionando mixins

--- src/services/PessoaServices.js
+++ src/services/PessoaServices.js
@@ -4,6 +4,12 @@ class PessoaServices extends Services {
   constructor() {
     super('Pessoa');
   }
+
+  async pegaMatriculasPorEstudante(estudanteId) {
+    const estudante = await super.pegaUmRegistroPorId(id);
+    const listaMatriculas = await estudante.getAulasMatriculadas();
+    return listaMatriculas;
+  }
 }
 
 module.exports = PessoaServices;
